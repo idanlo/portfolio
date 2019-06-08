83df5bff04b451b00cdc3682384865b06c7776c4
idanlo <idan2110@gmail.com>
2019-06-08 13:07:10 +0300

    remove commented code

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -58,13 +58,15 @@ const Helmet = ({ description, lang, meta }) => {
           content: metaDescription,
         },
         {
-          name: "twitter:image",
-          content: "https://raw.githubusercontent.com/idanlo/portfolio/master/media/IdanLo.png"
+          name: 'twitter:image',
+          content:
+            'https://raw.githubusercontent.com/idanlo/portfolio/master/media/IdanLo.png',
         },
         {
-          name: "twitter:image:src",
-          content: "https://raw.githubusercontent.com/idanlo/portfolio/master/media/IdanLo.png"
-        }
+          name: 'twitter:image:src',
+          content:
+            'https://raw.githubusercontent.com/idanlo/portfolio/master/media/IdanLo.png',
+        },
       ].concat(meta)}
     >
       <link
@@ -76,87 +78,6 @@ const Helmet = ({ description, lang, meta }) => {
     </ReactHelmet>
   );
 };
-// <StaticQuery
-//   query={graphql`
-//     query HelmetQuery {
-//       contentfulAbout {
-//         name
-//         description
-//         profile {
-//           favicon16: resize(width: 16) {
-//             src
-//           }
-//           favicon32: resize(width: 32) {
-//             src
-//           }
-//           bigIcon: resize(width: 192) {
-//             src
-//           }
-//           appleIcon: resize(width: 180) {
-//             src
-//           }
-//         }
-//       }
-//     }
-//   `}
-//   render={data => {
-//     const { name, description, profile } = data.contentfulAbout;
-//     const title = `${name} Portofolio`;
-
-//     return (
-//       <ReactHelmet>
-//         <meta charSet="utf-8" />
-//         <title>{title}</title>
-//         <meta name="description" content={description} />
-//         <link rel="shortcut icon" href={`https:${profile.favicon32.src}`} />
-//         <meta name="theme-color" content={theme.background} />
-//         <meta name="image" content={`https:${profile.favicon32.src}`} />
-
-//         <meta itemProp="name" content={title} />
-//         <meta itemProp="description" content={description} />
-//         <meta itemProp="image" content={`https:${profile.favicon32.src}`} />
-
-//         <meta name="og:title" content={title} />
-//         <meta name="og:description" content={description} />
-//         <meta name="og:image" content={`https:${profile.bigIcon.src}`} />
-//         <meta name="og:site_name" content={title} />
-//         <meta name="og:locale" content="en_US" />
-//         <meta name="og:type" content="website" />
-
-//         <meta name="twitter:card" content="summary" />
-//         <meta name="twitter:title" content={title} />
-//         <meta name="twitter:description" content={description} />
-//         <meta name="twitter:image" content={`https:${profile.bigIcon.src}`} />
-//         <meta name="twitter:image:src" content={`https:${profile.bigIcon.src}`} />
-
-//         <link
-//           rel="apple-touch-icon"
-//           sizes="180x180"
-//           href={`https:${profile.appleIcon.src}`}
-//         />
-//         <link
-//           rel="icon"
-//           type="image/png"
-//           sizes="32x32"
-//           href={`https:${profile.favicon32.src}`}
-//         />
-//         <link
-//           rel="icon"
-//           type="image/png"
-//           sizes="16x16"
-//           href={`https:${profile.favicon16.src}`}
-//         />
-
-//         <link
-//           href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-//           rel="stylesheet"
-//           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-//           crossOrigin="anonymous"
-//         />
-//       </ReactHelmet>
-//     );
-//   }}
-// />
 
 Helmet.defaultProps = {
   lang: `en`,
